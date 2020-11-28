f553cd91c17f488dced5758c3b3854772ca5b5a7
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-11-28 16:21:40 +0000

    Create Error() function

--- source/cli.h
+++ source/cli.h
@@ -1,9 +1,13 @@
 #ifndef __GAUSS_CLI_H__
 #define __GAUSS_CLI_H__
 
+void Error(char* msg) {
+	fprintf(stderr, msg);
+}
+//Error function
+
 void NullParamError() {
-	fprintf(
-		stderr,
+	Error (
 		"Gauss v1.0.0, the scriptable calculator.\n"
 		"No options selected, aborting...\n"
 		"\n"
